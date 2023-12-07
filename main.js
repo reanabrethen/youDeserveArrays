ae8a35d19cb949a5d02abe0d03c3b60092a0c3c0
Admin <reanajb@gmail.com>
2023-12-07 12:41:59 -0500

    all passing

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
 /******************
  * YOUR CODE HERE *
- ******************
+ ******************/
+
+function getFirstItemFrom(list){
+   return list[0];
+}
 
 
+function getLastItemFrom(list){
+  return (list[list.length -1]);
+}
 
 
+function getIndex3(list){
+    if(list.length < 4){
+      return list[list.length -1]
+}else{
+  return list[3]
+}
+}
+
+
+function isLongList(arr){
+  if(arr.length >= 10){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+function firstItemIsNumber(list){
+  if(typeof list[0] === "number"){
+    return true
+  }else{
+    return false
+  }
+}
+
+
+function secondCharOfThirdString(arr){
+  if(typeof arr[2][1] === "string"){
+    return arr[2][1]
+    }else{
+    return false
+  }
+}
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
